750c25097100743af55522eeb647d3f3ed335e26
Mymint <melarbi@emich.edu>
2024-02-22 16:18:48 -0500

    Adding the react components

---
+++ frontend/src/components/logine/Logine.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+export class Logine extends Component{
+    render(){
+        return(
+            <h1>Test it works</h1>
+        )
+    }
+}
---
+++ frontend/src/components/registerc/Registerc.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+export class Registerc extends Component{
+    render(){
+        return(
+            <h1>Test it works</h1>
+        )
+    }
+}
