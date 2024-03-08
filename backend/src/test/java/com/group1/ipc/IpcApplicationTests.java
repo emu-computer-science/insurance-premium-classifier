3cc78f3ac8213cd68d9361800ffce5bf31243d37
seanpolid <seanpolidori0@gmail.com>
2024-03-07 23:17:40 -0500

    Created skeleton and adjusted migration script

--- backend/src/test/java/com/group1/ipc/IpcApplicationTests.java
+++ backend/src/test/java/com/group1/ipc/IpcApplicationTests.java
@@ -2,8 +2,10 @@ package com.group1.ipc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IpcApplicationTests {
 
 	@Test
