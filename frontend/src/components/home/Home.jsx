222ec4e4780bf44a99f1cb4a782586ed75f13c7a
Saranyadhevi <saranyadhevi.rajasekaran@gmail.com>
2024-03-14 12:20:19 -0400

    Home frontend

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,28 +1,21 @@
-import './Home.css'
 import { Link } from 'react-router-dom';
+import styles from'./Home.module.css';
+import {Navbar1} from '../navbar1/Navbar1.jsx';
 
 const Home= () =>{
-    
     return (
-        
-        <>
-        
-        <div className="container">
+        <div>
+        <div className={styles.pinkEllipse}></div>
+        <div className={styles.blueCircle}></div>
+        <Navbar1/>
         <h1>Welcome to the Dashboard</h1>
-        <p>Choose an option:</p>
-        <Link className="btn" to="/login">Login</Link>
-        <Link className="btn" to="/sign-up">Signup</Link>
-        
+        <h2>Choose an option:</h2>
+        <div className={styles.container}>
+        <Link className={styles.btn} to="/login">Login</Link> 
+        <Link className={styles.btn} to="/sign-up">Signup</Link>
         </div> 
-        
-        
-        </>
-
-
-
+        </div>
     )
-
-
 }
 
 export default Home
