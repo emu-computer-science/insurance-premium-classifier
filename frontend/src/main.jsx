d57304af8f497a386eb15c02edbc73d20278ab2f
seanpolid <seanpolidori0@gmail.com>
2024-04-06 13:15:03 -0400

    Removed unnecessary context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import UserContextProvider from './stores/UserContextProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <UserContextProvider>
-    <App />
-  </UserContextProvider>,
+    <App />,
 )
