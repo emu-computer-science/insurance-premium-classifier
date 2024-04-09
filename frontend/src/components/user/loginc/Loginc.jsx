247ab969aae9bdacc69f639700b2c7c1f1cd97eb
seanpolid <seanpolidori0@gmail.com>
2024-04-09 10:40:16 -0400

    Made it so error message is shown on failed login

--- frontend/src/components/user/loginc/Loginc.jsx
+++ frontend/src/components/user/loginc/Loginc.jsx
@@ -5,69 +5,73 @@ import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Loginc = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const userContext = useContext(UserContext);
-  const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const userContext = useContext(UserContext);
+    const navigate = useNavigate();
 
-  const handleEmailChange  = (event) => {
-    setEmail(event.target.value);
-  }
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  }
-
-  const handleClickLogin = async () => {
-    const formData = new URLSearchParams();
-    formData.append('email', email);
-    formData.append('password', password);
-
-    const result = await userContext.login(formData);
-    if (result[0]) {
-      navigate('/user/dashboard')
+    const handleEmailChange  = (event) => {
+        setEmail(event.target.value);
     }
-  };
-  
-  const handleClickRegister = () => {
-    navigate('/sign-up');
-  };
 
-  return (
-      <div className="div">
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const handleClickLogin = async () => {
+        const formData = new URLSearchParams();
+        formData.append('email', email);
+        formData.append('password', password);
+
+        const result = await userContext.login(formData);
+        if (result[0]) {
+            navigate('/user/dashboard')
+        } else {
+            setErrorMessage('Username or password incorrect.');
+        }
+    }
+
+    const handleClickRegister = () => {
+        navigate('/sign-up');
+    };
+
+    return (
+        <div className="div">
         <Navbar1/>
         <div className={styles.pinkElipse}></div>
         <div className={styles.blueCircle}></div>
 
         <form className={styles.loginForm}>
-          <div>
+            {errorMessage ? <p>{errorMessage}</p> : null}
+            <div>
             <label>Email:</label>
             <input 
-              type="email" 
-              className={styles.inputField} 
-              placeholder="Email" 
-              value={email}
-              onChange={handleEmailChange}
+                type="email" 
+                className={styles.inputField} 
+                placeholder="Email" 
+                value={email}
+                onChange={handleEmailChange}
             />
-          </div>
-          
-          <div>
+            </div>
+            
+            <div>
             <label>Password:</label>
             <input 
-              type="password" 
-              className={styles.inputField} 
-              placeholder="Password" 
-              value={password}
-              onChange={handlePasswordChange}
+                type="password" 
+                className={styles.inputField} 
+                placeholder="Password" 
+                value={password}
+                onChange={handlePasswordChange}
             />
-          </div>
-          
-          <button type="button" className={styles.loginButton} onClick={handleClickLogin} >Login</button>
-          <div className={styles.text}>Forgot password?</div>
-          <button type="button" className={styles.registerButton} onClick={handleClickRegister} >Register</button>
+            </div>
+            
+            <button type="button" className={styles.loginButton} onClick={handleClickLogin} >Login</button>
+            <div className={styles.text}>Forgot password?</div>
+            <button type="button" className={styles.registerButton} onClick={handleClickRegister} >Register</button>
         </form>
-      </div>
-  );
+        </div>
+    );
 }
 
 export default Loginc;
